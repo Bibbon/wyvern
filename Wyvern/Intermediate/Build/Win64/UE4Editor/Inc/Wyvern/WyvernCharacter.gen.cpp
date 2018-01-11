4de6899a47d260423e20f7e960eff1b099f5a186
Bibbon <simlarouche@gmail.com>
2018-01-11 01:42:01 -0500

    Initial UE4 commit

---
+++ Wyvern/Intermediate/Build/Win64/UE4Editor/Inc/Wyvern/WyvernCharacter.gen.cpp
@@ -0,0 +1,108 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "GeneratedCppIncludes.h"
+#include "WyvernCharacter.h"
+#ifdef _MSC_VER
+#pragma warning (push)
+#pragma warning (disable : 4883)
+#endif
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+void EmptyLinkFunctionForGeneratedCodeWyvernCharacter() {}
+// Cross Module References
+	WYVERN_API UClass* Z_Construct_UClass_AWyvernCharacter_NoRegister();
+	WYVERN_API UClass* Z_Construct_UClass_AWyvernCharacter();
+	ENGINE_API UClass* Z_Construct_UClass_ACharacter();
+	UPackage* Z_Construct_UPackage__Script_Wyvern();
+	ENGINE_API UClass* Z_Construct_UClass_UDecalComponent_NoRegister();
+	ENGINE_API UClass* Z_Construct_UClass_USpringArmComponent_NoRegister();
+	ENGINE_API UClass* Z_Construct_UClass_UCameraComponent_NoRegister();
+// End Cross Module References
+	void AWyvernCharacter::StaticRegisterNativesAWyvernCharacter()
+	{
+	}
+	UClass* Z_Construct_UClass_AWyvernCharacter_NoRegister()
+	{
+		return AWyvernCharacter::StaticClass();
+	}
+	UClass* Z_Construct_UClass_AWyvernCharacter()
+	{
+		static UClass* OuterClass = nullptr;
+		if (!OuterClass)
+		{
+			static UObject* (*const DependentSingletons[])() = {
+				(UObject* (*)())Z_Construct_UClass_ACharacter,
+				(UObject* (*)())Z_Construct_UPackage__Script_Wyvern,
+			};
+#if WITH_METADATA
+			static const UE4CodeGen_Private::FMetaDataPairParam Class_MetaDataParams[] = {
+				{ "BlueprintType", "true" },
+				{ "HideCategories", "Navigation" },
+				{ "IncludePath", "WyvernCharacter.h" },
+				{ "IsBlueprintBase", "true" },
+				{ "ModuleRelativePath", "WyvernCharacter.h" },
+			};
+#endif
+#if WITH_METADATA
+			static const UE4CodeGen_Private::FMetaDataPairParam NewProp_CursorToWorld_MetaData[] = {
+				{ "AllowPrivateAccess", "true" },
+				{ "Category", "Camera" },
+				{ "EditInline", "true" },
+				{ "ModuleRelativePath", "WyvernCharacter.h" },
+				{ "ToolTip", "A decal that projects to the cursor location." },
+			};
+#endif
+			static const UE4CodeGen_Private::FObjectPropertyParams NewProp_CursorToWorld = { UE4CodeGen_Private::EPropertyClass::Object, "CursorToWorld", RF_Public|RF_Transient|RF_MarkAsNative, 0x00400000000a001d, 1, nullptr, STRUCT_OFFSET(AWyvernCharacter, CursorToWorld), Z_Construct_UClass_UDecalComponent_NoRegister, METADATA_PARAMS(NewProp_CursorToWorld_MetaData, ARRAY_COUNT(NewProp_CursorToWorld_MetaData)) };
+#if WITH_METADATA
+			static const UE4CodeGen_Private::FMetaDataPairParam NewProp_CameraBoom_MetaData[] = {
+				{ "AllowPrivateAccess", "true" },
+				{ "Category", "Camera" },
+				{ "EditInline", "true" },
+				{ "ModuleRelativePath", "WyvernCharacter.h" },
+				{ "ToolTip", "Camera boom positioning the camera above the character" },
+			};
+#endif
+			static const UE4CodeGen_Private::FObjectPropertyParams NewProp_CameraBoom = { UE4CodeGen_Private::EPropertyClass::Object, "CameraBoom", RF_Public|RF_Transient|RF_MarkAsNative, 0x00400000000a001d, 1, nullptr, STRUCT_OFFSET(AWyvernCharacter, CameraBoom), Z_Construct_UClass_USpringArmComponent_NoRegister, METADATA_PARAMS(NewProp_CameraBoom_MetaData, ARRAY_COUNT(NewProp_CameraBoom_MetaData)) };
+#if WITH_METADATA
+			static const UE4CodeGen_Private::FMetaDataPairParam NewProp_TopDownCameraComponent_MetaData[] = {
+				{ "AllowPrivateAccess", "true" },
+				{ "Category", "Camera" },
+				{ "EditInline", "true" },
+				{ "ModuleRelativePath", "WyvernCharacter.h" },
+				{ "ToolTip", "Top down camera" },
+			};
+#endif
+			static const UE4CodeGen_Private::FObjectPropertyParams NewProp_TopDownCameraComponent = { UE4CodeGen_Private::EPropertyClass::Object, "TopDownCameraComponent", RF_Public|RF_Transient|RF_MarkAsNative, 0x00400000000a001d, 1, nullptr, STRUCT_OFFSET(AWyvernCharacter, TopDownCameraComponent), Z_Construct_UClass_UCameraComponent_NoRegister, METADATA_PARAMS(NewProp_TopDownCameraComponent_MetaData, ARRAY_COUNT(NewProp_TopDownCameraComponent_MetaData)) };
+			static const UE4CodeGen_Private::FPropertyParamsBase* const PropPointers[] = {
+				(const UE4CodeGen_Private::FPropertyParamsBase*)&NewProp_CursorToWorld,
+				(const UE4CodeGen_Private::FPropertyParamsBase*)&NewProp_CameraBoom,
+				(const UE4CodeGen_Private::FPropertyParamsBase*)&NewProp_TopDownCameraComponent,
+			};
+			static const FCppClassTypeInfoStatic StaticCppClassTypeInfo = {
+				TCppClassTypeTraits<AWyvernCharacter>::IsAbstract,
+			};
+			static const UE4CodeGen_Private::FClassParams ClassParams = {
+				&AWyvernCharacter::StaticClass,
+				DependentSingletons, ARRAY_COUNT(DependentSingletons),
+				0x00800080u,
+				nullptr, 0,
+				PropPointers, ARRAY_COUNT(PropPointers),
+				nullptr,
+				&StaticCppClassTypeInfo,
+				nullptr, 0,
+				METADATA_PARAMS(Class_MetaDataParams, ARRAY_COUNT(Class_MetaDataParams))
+			};
+			UE4CodeGen_Private::ConstructUClass(OuterClass, ClassParams);
+		}
+		return OuterClass;
+	}
+	IMPLEMENT_CLASS(AWyvernCharacter, 2149419074);
+	static FCompiledInDefer Z_CompiledInDefer_UClass_AWyvernCharacter(Z_Construct_UClass_AWyvernCharacter, &AWyvernCharacter::StaticClass, TEXT("/Script/Wyvern"), TEXT("AWyvernCharacter"), false, nullptr, nullptr, nullptr);
+	DEFINE_VTABLE_PTR_HELPER_CTOR(AWyvernCharacter);
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+#ifdef _MSC_VER
+#pragma warning (pop)
+#endif
---
+++ Wyvern/Intermediate/Build/Win64/UE4Editor/Inc/Wyvern/WyvernPlayerController.generated.h
@@ -0,0 +1,90 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "ObjectMacros.h"
+#include "ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef WYVERN_WyvernPlayerController_generated_h
+#error "WyvernPlayerController.generated.h already included, missing '#pragma once' in WyvernPlayerController.h"
+#endif
+#define WYVERN_WyvernPlayerController_generated_h
+
+#define Wyvern_Source_Wyvern_WyvernPlayerController_h_12_RPC_WRAPPERS
+#define Wyvern_Source_Wyvern_WyvernPlayerController_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define Wyvern_Source_Wyvern_WyvernPlayerController_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesAWyvernPlayerController(); \
+	friend WYVERN_API class UClass* Z_Construct_UClass_AWyvernPlayerController(); \
+public: \
+	DECLARE_CLASS(AWyvernPlayerController, APlayerController, COMPILED_IN_FLAGS(0 | CLASS_Config), 0, TEXT("/Script/Wyvern"), NO_API) \
+	DECLARE_SERIALIZER(AWyvernPlayerController) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define Wyvern_Source_Wyvern_WyvernPlayerController_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesAWyvernPlayerController(); \
+	friend WYVERN_API class UClass* Z_Construct_UClass_AWyvernPlayerController(); \
+public: \
+	DECLARE_CLASS(AWyvernPlayerController, APlayerController, COMPILED_IN_FLAGS(0 | CLASS_Config), 0, TEXT("/Script/Wyvern"), NO_API) \
+	DECLARE_SERIALIZER(AWyvernPlayerController) \
+	enum {IsIntrinsic=COMPILED_IN_INTRINSIC};
+
+
+#define Wyvern_Source_Wyvern_WyvernPlayerController_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API AWyvernPlayerController(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(AWyvernPlayerController) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AWyvernPlayerController); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AWyvernPlayerController); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AWyvernPlayerController(AWyvernPlayerController&&); \
+	NO_API AWyvernPlayerController(const AWyvernPlayerController&); \
+public:
+
+
+#define Wyvern_Source_Wyvern_WyvernPlayerController_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API AWyvernPlayerController(AWyvernPlayerController&&); \
+	NO_API AWyvernPlayerController(const AWyvernPlayerController&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, AWyvernPlayerController); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(AWyvernPlayerController); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(AWyvernPlayerController)
+
+
+#define Wyvern_Source_Wyvern_WyvernPlayerController_h_12_PRIVATE_PROPERTY_OFFSET
+#define Wyvern_Source_Wyvern_WyvernPlayerController_h_9_PROLOG
+#define Wyvern_Source_Wyvern_WyvernPlayerController_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Wyvern_Source_Wyvern_WyvernPlayerController_h_12_PRIVATE_PROPERTY_OFFSET \
+	Wyvern_Source_Wyvern_WyvernPlayerController_h_12_RPC_WRAPPERS \
+	Wyvern_Source_Wyvern_WyvernPlayerController_h_12_INCLASS \
+	Wyvern_Source_Wyvern_WyvernPlayerController_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define Wyvern_Source_Wyvern_WyvernPlayerController_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	Wyvern_Source_Wyvern_WyvernPlayerController_h_12_PRIVATE_PROPERTY_OFFSET \
+	Wyvern_Source_Wyvern_WyvernPlayerController_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	Wyvern_Source_Wyvern_WyvernPlayerController_h_12_INCLASS_NO_PURE_DECLS \
+	Wyvern_Source_Wyvern_WyvernPlayerController_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID Wyvern_Source_Wyvern_WyvernPlayerController_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
